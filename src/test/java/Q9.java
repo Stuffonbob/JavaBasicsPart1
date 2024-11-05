42af2633ea0b13eed80bf23f8a5e3c26d0bf20b9
ziad-cpu <hziademp@gmail.com>
2024-11-05 13:45:37 -0500

    Question 9

--- src/test/java/Q9.java
+++ src/test/java/Q9.java
@@ -1,5 +1,31 @@
 public class Q9 {
     public static void main(String[] args) {
 
+        String [] array = {"john", "michael", "chris", "jessica", "john", "drew", "lonzo", "drew", "john" };
+
+        System.out.println("Print out every incident where a name duplicates: ");
+
+        for (String name : array) {
+            int counter = 0;
+
+            for (int j = 0; j < array.length; j++) {
+
+                String comparison = array[j];
+
+
+                if (name.equals(comparison)) {
+                    counter++;
+                    if (counter == 2 ) {
+                        System.out.print(name + ", ");
+                    }
+                }
+
+
+            }
+
+
+
+
+        }
     }
 }
