ae262559145aeef9692f5ed1e744e7aba6955c64
ziad-cpu <hziademp@gmail.com>
2024-11-04 20:12:23 -0500

    Q1

--- src/test/java/Q1.java
+++ src/test/java/Q1.java
@@ -1,5 +1,20 @@
 public class Q1 {
     public static void main(String[] args) {
+        int [] temp = {12, 42, 105, 31, 21, 11, 52};
 
+        int max = temp[0];
+        int min = temp[0];
+
+        for (int t : temp) {
+
+            if (t > max) {
+                max = t;
+            }
+            if (t < min) {
+                min = t;
+            }
+        }
+        System.out.println("max temp " + max);
+        System.out.println("min temp " + min);
     }
 }
