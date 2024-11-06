07b563d8157c2331297026292a34a3b9946eb425
Stuffonbob <juancarer@gmail.com>
2024-11-05 18:22:43 -0600

    Q6

--- src/test/java/Q6.java
+++ src/test/java/Q6.java
@@ -1,9 +1,45 @@
 /*
 Write a Java program to check whether a given number is prime or not.
  */
+
+import java.util.Scanner;
+
 public class Q6 {
     public static void main(String[] args) {
 
+        //set up a scanner to ask user for a number to check
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input a number to check if it is prime or not: ");
+        int num = scanner.nextInt();
 
+        //set up boolean for conditional statements
+        boolean prime = true;
+        /*
+        a prime numbers can only be divided by itself and 1
+        if it can be divided by any number other than those, it is not prime
+         */
+        for (int x = 2; x <= num / 2; x++) {
+            // condition for nonprime number
+            if (num % x == 0) {
+                prime = false;
+                break;
+            }
+        }
+        //can't check 0 or 1 with the for loop so define them here
+        if(num==0 || num==1){
+            prime=false;
+        }
+
+        if (prime) {
+            System.out.println("The number: " + num + " is prime");
+        } else {
+            System.out.println("The number: " + num + " is not prime");
+        }
     }
 }
+
+
+
+
+
+
